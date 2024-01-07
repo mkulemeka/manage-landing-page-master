a8054d1a8ed730319898da2ef19daca7401d361b
Mtendere Kulemeka <kahlekulemeka@gmail.com>
2024-01-07 12:56:50 -0500

    made landing/hero responsive

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ styles }) => {
   return (
     <div className="action_button">
       <button
-        className={`p-2 rounded-3xl ${styles} text-12 font-500 w-[7.5rem] shadow-sm shadow-brightRed transition-opacity duration-500 ease-in-out hover:opacity-70`}
+        className={`p-3 rounded-3xl ${styles} text-12 font-500 w-[7.5rem] shadow-sm shadow-brightRed transition-opacity duration-500 ease-in-out hover:opacity-70`}
       >
         Get Started
       </button>
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -3,18 +3,18 @@ import IllustrationIntro from "../images/illustration-intro.svg";
 
 const Landing = () => {
   return (
-    <main className="hero_section flex justify-between my-12">
-      <section className="page_intro w-[50%] flex flex-col justify-center gap-9">
-        <h1 className="page_title font-700 text-[3.5rem] leading-[3.5rem] text-darkBlue">
+    <main className="hero_section flex flex-col-reverse md:flex-row justify-between md:my-12">
+      <section className="page_intro md:w-[50%] flex flex-col justify-center items-center md:items-start gap-5 md:gap-9">
+        <h1 className="page_title font-700 text-[2.5rem] leading-10 text-center md:text-left md:text-[3.5rem] md:leading-[3.5rem] text-darkBlue">
           Bring everyone together to build better products.
         </h1>
-        <p className="page_description text-16 max-w-[55%] text-darkGrayishBlue">
+        <p className="page_description text-16 text-center md:text-left md:max-w-[55%] text-darkGrayishBlue">
           Manage makes it simple for software teams to plan day-to-day tasks
           while keeping the larger team goals in view.
         </p>
         <Button styles="bg-brightRed text-veryLightGray" />
       </section>
-      <section className="page_image w-[50%]">
+      <section className="page_image md:w-[50%]">
         <img src={IllustrationIntro} alt="page illustration" />
       </section>
     </main>
